2cc333e1786f3487224110879356e461726e5978
Sarim Ahmed <50093514+sarim2000@users.noreply.github.com>
2025-05-30 00:06:21 +0530

    Update parameter.ts

--- cdk/parameter.ts
+++ cdk/parameter.ts
@@ -5,4 +5,13 @@ export const bedrockChatParams = new Map<string, BedrockChatParametersInput>();
 // bedrockChatParams.set("dev", {});
 
 // If you define "default" environment here, parameters in cdk.json are ignored
-// bedrockChatParams.set("default", {});
+bedrockChatParams.set("prod", {
+  // envName will be 'bedrockv3-poc' and envPrefix will also be 'bedrockv3-poc'
+  bedrockRegion: "us-east-1", // Or your desired region for this POC
+  userPoolDomainPrefix: "kemper-bedrockv3-poc", // VALID: Globally unique Cognito prefix
+  allowedSignUpEmailDomains: ["kemper.com"],   // VALID: Restrict sign-ups to kemper.com emails
+  selfSignUpEnabled: true,                     // Example for a POC
+  enableRagReplicas: false,                    // For a POC, you might not need replicas
+  // ... fill in other necessary parameters from BedrockChatParametersInput
+  // or let them take defaults from the schema.
+});
